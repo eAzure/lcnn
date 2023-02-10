68d0eaa976e12e5cb108673396f4ea06d8cef87d
eAzure <1115198434@qq.com>
2023-02-10 15:01:56 +0800

    feat(googletest): add googletest

---
+++ tests/test_util/test_gtest.cc
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+namespace {
+
+/**
+ * 用于测试gtest是否已经生效
+*/
+TEST(UtilsTest, Gtest) {
+    // The test is named "Gtest", and belongs to the "UtilsTest" test case.
+    EXPECT_FALSE(false);
+}
+
+} // namespace
