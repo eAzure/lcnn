b33754122d4c82aea5808342ae3fb73c2fd663bf
eAzure <1115198434@qq.com>
2023-02-11 15:41:38 +0800

    update(log): cancel spdlog

--- src/tensor/tensor.cc
+++ src/tensor/tensor.cc
@@ -3,15 +3,13 @@
  * [by lgx 2023-02-10]
 */
 
-#include "spdlog/spdlog.h"
 #include "tensor/tensor.h"
 
 namespace lcnn {
 
 // temp for test
 void Tensor<float>::test() {
-    spdlog::info("Welcome to spdlog!");
-    spdlog::info("tensor's rows is {}", _data.dimensions()[1]);
+    std::cout << "tensor's rows is " << _data.dimensions()[1] << std::endl;
 }
 
 Tensor<float>::Tensor(uint32_t channels, uint32_t rows, uint32_t cols) {
