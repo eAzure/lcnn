562a9226b541e0aa049a3dd8530f570c94dd2106
eAzure <1115198434@qq.com>
2023-02-25 22:24:48 +0800

    add(operand): add operand and data_type

---
+++ include/ir/operand.h
@@ -0,0 +1,29 @@
+/**
+ * operand.h
+ * 操作数
+ * [by lgx 2023-02-25]
+*/
+
+#ifndef _LCNN_IR_OPERAND_H
+#define _LCNN_IR_OPERAND_H
+
+#include "tensor/tensor.h"
+#include "utils/data_type.h"
+
+namespace lcnn {
+
+struct Operand
+{
+    // 操作数名称
+    std::string name;
+    // 操作数形状
+    std::vector<uint32_t> shapes;
+    // 对应的Tensor数据
+    std::vector<std::shared_ptr<Tensor<float>>> datas;
+    // 操作数类型
+    OperandDataType type = OperandDataType::kTypeUnknown;
+};
+
+} // namespace lcnn
+
+#endif // _LCNN_IR_OPERAND_H
---
+++ include/utils/data_type.h
@@ -0,0 +1,28 @@
+/**
+ * data_type.h
+ * 相关数据类型
+ * [by lgx 2023-02-25]
+*/
+
+#ifndef _LCNN_UTILS_DATA_TYPE_H
+#define _LCNN_UTILS_DATA_TYPE_H
+
+namespace lcnn {
+
+// 操作数对应的数据类型
+enum class OperandDataType {
+    kTypeUnknown = 0, // 类型未知
+    kTypeFloat32 = 1, // float32
+};
+
+// 操作数参数对应的数据类型
+enum class OperatorParameterType {
+    kTypeUnknown = 0, // 类型未知
+    kTypeBool = 1, // bool
+    kTypeInt = 2, // int
+    kTypeFloat = 3 // float
+};
+
+} // namespace lcnn
+
+#endif // _LCNN_UTILS_DATA_TYPE_H
